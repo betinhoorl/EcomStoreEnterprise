ced6efdfee68198687d98ad2b471f10dbaef1102
roberto.silva <roberto.silva@luizalabs.com>
2021-02-09 11:49:22 -0300

    Start Project

---
+++ src/BuildingBlock/ESE.Core/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace ESE.Core
+{
+    public class Class1
+    {
+    }
+}
---
+++ src/Web/ESE.Webapp.MVC/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace ESE.Webapp.MVC.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
