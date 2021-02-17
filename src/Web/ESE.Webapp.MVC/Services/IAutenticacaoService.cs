d52560f6445ae82aeeabea41ce3a25dfe1cb1127
roberto.silva <roberto.silva@luizalabs.com>
2021-02-17 13:12:21 -0300

    Web App project

---
+++ src/Web/ESE.Webapp.MVC/Services/IAutenticacaoService.cs
@@ -0,0 +1,12 @@
+﻿using System.Threading.Tasks;
+using ESE.Webapp.MVC.Models;
+
+namespace ESE.Webapp.MVC.Services
+{
+    public interface IAutenticacaoService
+    {
+        Task<UsuarioRespostaLogin> Login(UsuarioLogin usuarioLogin);
+
+        Task<UsuarioRespostaLogin> Registro(UsuarioRegistro usuarioRegistro);
+    }
+}
